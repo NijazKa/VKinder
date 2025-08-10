1ba114b16be533da98c7bf5f9f96b521d800ca73
Nijaz <981475@gmail.com>
2025-08-10 17:26:50 +0300

    version 1.02

--- main.py
+++ main.py
@@ -22,9 +22,34 @@ keyboard.add_button('Назад', color=VkKeyboardColor.POSITIVE)
 keyboard.add_button('В избранное', color=VkKeyboardColor.POSITIVE)
 
 
-def sender(id, text): # функция отправки сообщения от бота к пользователю
-    vk.messages.send(user_id=id, message=text, random_id=randrange(10 ** 7), keyboard=keyboard.get_keyboard())
+def sender(user_id, text): # функция отправки сообщения от бота к пользователю + клавиатура
+    vk.messages.send(user_id=user_id, message=text, random_id=randrange(10 ** 7), keyboard=keyboard.get_keyboard())
 
+# функция получения трех фотографий пользователя с наибольшим количеством лайков
+def top_photo(user_id):
+    albums = vk.photos.getAlbums(owner_id=user_id)
+    photos = []
+    # Проходим по каждому альбому и получаем фотографии
+    for album in albums['items']:
+        album_id = album['id']
+        # Получаем фотографии из альбома
+        photos_in_album = vk.photos.get(owner_id=user_id, album_id=album_id, count=1000)
+        for photo in photos_in_album['items']:
+            # Добавляем фотографию и количество лайков в список
+            photos.append({
+                'url': photo['sizes'][-1]['url'],
+                'likes': photo['likes']['count']
+            })
+
+    # Сортируем фотографии по количеству лайков и выбираем три с наибольшим количеством
+    top_photos = sorted(photos, key=lambda x: x['likes'], reverse=True)[:3]
+
+    # Выводим найденные фотографии
+    for photo in top_photos:
+        print(f"URL: {photo['url']}, Likes: {photo['likes']}") #пока печать, потом записать в базу данных
+
+
+### БАЗОВЫЙ ЦИКЛ ЗАПУСКА БОТА
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
