a85070cfa14e66bb21d452fa0fa2d12a2719bc2b
Nijaz <981475@gmail.com>
2025-08-10 17:01:54 +0300

    version 1.01

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 import json
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from random import randrange
 
 from config import TOKEN
 
-
+# Блок авторизации
 vk_session = vk_api.VkApi(token=TOKEN)
 vk = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
+# Блок авторизации
 
-def sender(id, text):
-    vk.messages.send(user_id=id, message=text, random_id=0)
+# def write_msg(user_id, message):
+#     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+# Создание клавиатуры
+keyboard = VkKeyboard(one_time=True)
+keyboard.add_button('Вперед', color=VkKeyboardColor.SECONDARY)
+keyboard.add_button('Назад', color=VkKeyboardColor.POSITIVE)
+keyboard.add_button('В избранное', color=VkKeyboardColor.POSITIVE)
+
+
+def sender(id, text): # функция отправки сообщения от бота к пользователю
+    vk.messages.send(user_id=id, message=text, random_id=randrange(10 ** 7), keyboard=keyboard.get_keyboard())
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
         if event.to_me:
-            msg = event.text.lower()
-            id = event.user_id
-            if msg == 'hi':
-                sender(id, 'hello')
+            request = event.text.lower()
+            if request == "привет":
+                sender(event.user_id, f"Хай, {event.user_id}")
+            elif request == "пока":
+                sender(event.user_id, "Пока((")
+            else:
+                sender(event.user_id, "Не поняла вашего ответа...")
